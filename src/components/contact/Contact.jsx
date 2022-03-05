2c131d59120cc9ca1e17057bbe6064e3036b9eb0
Leo <ant.zeron@gmail.com>
2022-03-05 22:20:07 +0900

    Css changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,70 +19,70 @@ function Contact() {
 
     return (
         <Container>
-        <div>
-           <div className='features-header'>
-              <h1 className="gradient__text center-title">Contact</h1>
-              <h4 className='gradient__text center-title'>Get the answers to life</h4>
-           </div>
-           < br/>
-           <Form className='contact-form'>
-              <Row>
-                 <Col>
-                 <Form.Group className="mb-3 form-control-plaintext">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="First Name" />
-                 </Form.Group>
-                 </Col>
-                 <Col>
-                 <Form.Group className="mb-3 form-control-plaintext">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Last Name" />
-                 </Form.Group>
-                 </Col>
-              </Row>
-              <Row>
-                 <Col>
-                 <Form.Group className="mb-3 form-control-plaintext" controlId="formBasicEmail">
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="phone" placeholder="Phone number" />
-                 </Form.Group>
-                 </Col>
-                 <Col>
-                 <Form.Group className="mb-3 form-control-plaintext" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                 </Form.Group>
-                 </Col>
-              </Row>
-              <Row>
-                 <Col>
-                 <Form.Group className="mb-3 text-description form-control-plaintext">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" placeholder="Describe your Issue" />
-                 </Form.Group>
-                 </Col>
-              </Row>
-              < br/>
-              <div className='submit-button-center'>
-                 <Button className='button-contact' type="clear">
-                 CLEAR
-                 </Button>
-                 <Button className='button-contact'  onClick={handleShow}>
-                 SUBMIT
-                 </Button>
-                 <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                    </Modal.Header>
-                    <Modal.Body>< Message /></Modal.Body>
-                    <Modal.Footer>
-                       <Button variant="secondary" onClick={handleClose}>
-                       Close
-                       </Button>
-                    </Modal.Footer>
-                 </Modal>
-              </div>
-           </Form>
-        </div>
+            <div>
+               <div className='features-header'>
+                  <h1 className="gradient__text center-title">Contact</h1>
+                  <h4 className='gradient__text center-title'>Get the answers to life</h4>
+               </div>
+               < br/>
+               <Form className='contact-form'>
+                  <Row>
+                     <Col>
+                     <Form.Group className="mb-3 form-control__plaintext">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="text" placeholder="First Name" />
+                     </Form.Group>
+                     </Col>
+                     <Col>
+                     <Form.Group className="mb-3 form-control-plaintext">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="text" placeholder="Last Name" />
+                     </Form.Group>
+                     </Col>
+                  </Row>
+                  <Row>
+                     <Col>
+                     <Form.Group className="mb-3 form-control-plaintext" controlId="formBasicEmail">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control type="phone" placeholder="Phone number" />
+                     </Form.Group>
+                     </Col>
+                     <Col>
+                     <Form.Group className="mb-3 form-control-plaintext" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                     </Form.Group>
+                     </Col>
+                  </Row>
+                  <Row>
+                     <Col>
+                     <Form.Group className="mb-3 text-description form-control-plaintext">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" placeholder="Describe your Issue" />
+                     </Form.Group>
+                     </Col>
+                  </Row>
+                  < br/>
+                  <div className='contact-form__submit-button'>
+                     <Button className='button-contact' type="clear">
+                     CLEAR
+                     </Button>
+                     <Button className='button-contact'  onClick={handleShow}>
+                     SUBMIT
+                     </Button>
+                     <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                        </Modal.Header>
+                        <Modal.Body>< Message /></Modal.Body>
+                        <Modal.Footer>
+                           <Button variant="secondary" onClick={handleClose}>
+                           Close
+                           </Button>
+                        </Modal.Footer>
+                     </Modal>
+                  </div>
+               </Form>
+            </div>
      </Container>
     );
   }; 
